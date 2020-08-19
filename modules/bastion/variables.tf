ff4bb6915f43cff70815fe4b596d52877b6e12cd
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 02:57:03 +0000

    Updated variables.tf

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -6,6 +6,10 @@ variable "region" {
   description = "The Azure location where all resources in this example should be created"
 }
 
+variable "subnet" {
+  description = "The subnet for all resources in this module"
+}
+
 ################################
 # publisher = "SUSE"
 # offer     = "sles-sap-12-sp5"
