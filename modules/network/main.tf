bb5d287ccb1937fc78fa26524e607f16ce0fa3f6
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 02:36:32 +0000

    Added main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = var.region
+  resource_group_name = var.rg
+  location            = var.region
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 =  var.region
+  virtual_network_name =  var.region
+  resource_group_name  =  var.rg
+  address_prefix       = "10.0.0.0/24"
+  depends_on = [ azurerm_virtual_network.network ]
+}
