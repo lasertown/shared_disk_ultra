fc1a58e76936e8ad38c9c1ce1a2ec4d1ee00b415
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 03:30:09 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ module "rg0" {
   source = "./modules/resource_group"
   rg = "test_cluster"
 }
+
+module "network0" {
+    source = "./modules/network"
+    rg = module.rg0.rg0
+    region = "westus2"
+}
+
+module "bastion0" {
+source = "./modules/bastion"
+rg = module.rg0.rg
+region = "westus2"
+publisher = "SUSE"
+offer = "sles-sap-12-sp5"
+sku = "gen2"
+_version = "latest"
+}
