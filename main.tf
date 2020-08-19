8f97ab99cf2b90a2284be7a5f885d1877259d230
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 19:45:03 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ offer = "sles-sap-12-sp5"
 sku = "gen2"
 _version = "latest"
 }
+
+module "nodes0" {
+source = "./modules/node_pair"
+rg = module.rg0.rg
+region = module.network0.region
+subnet = module.network0.subnet
+publisher = "SUSE"
+offer = "sles-sap-12-sp5"
+sku = "gen2"
+_version = "latest"
+}
