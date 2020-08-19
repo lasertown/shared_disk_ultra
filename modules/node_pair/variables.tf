5f20b197b01882eb0e88d826bdfd76e9ffa9077f
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 19:40:50 +0000

    Added variables.tf

---
+++ modules/node_pair/variables.tf
@@ -0,0 +1,35 @@
+variable "rg" {
+  description = "The Resource Group name for all resources in this module"
+}
+
+variable "region" {
+  description = "The Azure location where all resources in this example should be created"
+}
+
+variable "subnet" {
+  description = "The subnet for all resources in this module"
+}
+
+################################
+# publisher = "SUSE"
+# offer     = "sles-sap-12-sp5"
+# sku       = "gen2"
+# _version   = "latest"
+################################
+# publisher = "SUSE"
+# offer     = "SLES-SAP"
+# sku       = "12-sp4-gen2"
+# _version   = "latest"
+################################
+variable "publisher" {
+  description = "Publisher of the image used to create VM"
+}
+variable "offer" {
+  description = "Offer of the image used to create VM"
+}
+variable "sku" {
+  description = "SKU of the image used to create VM"
+}
+variable "_version" {
+  description = "Version of the image used to create VM, underscore added to avoid Terraform error"
+}
