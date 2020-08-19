038bdd30b22b6c6d37714396281a6d7e72287342
Jason Ju <Jason.Ju@microsoft.com>
2020-08-19 02:41:23 +0000

    Updated main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -2,21 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_virtual_network" "network" {
-  name                = var.region
-  resource_group_name = var.rg
-  location            = var.region
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 =  var.region
-  virtual_network_name =  var.region
-  resource_group_name  =  var.rg
-  address_prefix       = "10.0.0.0/24"
-  depends_on = [ azurerm_virtual_network.network ]
-}
-
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "ssh" {
     name                = var.region
