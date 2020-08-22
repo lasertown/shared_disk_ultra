d1a07d1a959133849a7aba3df0b8ba79154360c3
Jason Ju <Jason.Ju@microsoft.com>
2020-08-22 07:09:27 +0000

    Updated node.shared.config.py

--- node.config/node.shared.config.py
+++ node.config/node.shared.config.py
@@ -7,10 +7,5 @@ grep = subprocess.Popen(['grep', '512G'], stdin=lsblk.stdout, stdout=subprocess.
 disk = grep.stdout.readlines()
 id = []
 for i in disk:
-  ls = subprocess.Popen(['ls', '-la', '/dev/disk/by-id/'], stdout=subprocess.PIPE,)
-  grep2 = subprocess.Popen(['grep', i[0:3]], stdin=ls.stdout, stdout=subprocess.PIPE,)
-  grep3 = subprocess.Popen(['grep', 'scsi-3'], stdin=grep2.stdout, stdout=subprocess.PIPE,)
-  id.append('/dev/disk/by-id/' + grep3.stdout.read().split()[8])
-for i in id:
-#  subprocess.call(['sudo', 'sbd', '-d', i, '-1', '60', '-4', '120', 'create'])
-  print i
+  subprocess.call(['sudo', 'parted', '-s', '/dev/' + i.split()[0], 'mklabel', 'gpt'])
+  subprocess.call(['sudo', 'parted', '-s', '-a', 'opt', '/dev/' + i.split()[0], 'mkpart', 'extended', '0%', '100%'])
