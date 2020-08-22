a4f731c52b62c909dcac1199c38b59f7d7affacc
Jason Ju <Jason.Ju@microsoft.com>
2020-08-22 06:49:09 +0000

    Updated node.shared.config.py

--- node.config/node.shared.config.py
+++ node.config/node.shared.config.py
@@ -3,7 +3,7 @@
 import subprocess
 
 lsblk = subprocess.Popen(['lsblk'], stdout=subprocess.PIPE,)
-grep = subprocess.Popen(['grep', '256G'], stdin=lsblk.stdout, stdout=subprocess.PIPE,)
+grep = subprocess.Popen(['grep', '512G'], stdin=lsblk.stdout, stdout=subprocess.PIPE,)
 disk = grep.stdout.readlines()
 id = []
 for i in disk:
@@ -12,17 +12,5 @@ for i in disk:
   grep3 = subprocess.Popen(['grep', 'scsi-3'], stdin=grep2.stdout, stdout=subprocess.PIPE,)
   id.append('/dev/disk/by-id/' + grep3.stdout.read().split()[8])
 for i in id:
-  subprocess.call(['sudo', 'sbd', '-d', i, '-1', '60', '-4', '120', 'create'])
-e = open("/tmp/sbd", "a")
-f = open("/etc/sysconfig/sbd", "r")
-for i in (f.read().splitlines()):
-  if "#SBD_DEVICE=" in i:
-    e.write("SBD_DEVICE=" + "\"" + ";".join(id) + "\"" + "\n")
-  else:
-    e.write(i + "\n")
-e.close()
-f.close()
-subprocess.call(['cp', '/tmp/sbd', '/etc/sysconfig/sbd'])
-subprocess.call(['rm', '/tmp/sbd'])
-g = open("/etc/delete.to.retry.node.sbd.config.py", "w")
-g.close()
+#  subprocess.call(['sudo', 'sbd', '-d', i, '-1', '60', '-4', '120', 'create'])
+  print i
