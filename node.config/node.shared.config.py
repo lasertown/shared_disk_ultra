95c749511ba5b608f03c9a98ebad594795bebb37
Jason Ju <Jason.Ju@microsoft.com>
2020-08-22 09:38:52 +0000

    Updated node.shared.config.py

--- node.config/node.shared.config.py
+++ node.config/node.shared.config.py
@@ -9,7 +9,5 @@ disk = grep.stdout.readlines()
 id = []
 for i in disk:
   subprocess.call(['sudo', 'parted', '-s', '/dev/' + i.split()[0], 'mklabel', 'gpt'])
-  time.sleep(2)
-  subprocess.call(['sudo', 'parted', '-s', '-a', 'opt', '/dev/' + i.split()[0], 'mkpart', 'extended', '0%', '100%'])
-  time.sleep(2)
+  subprocess.call(['sudo', 'parted', '-s', '-a', 'opt', '/dev/' + i.split()[0], 'mkpart', 'extended', '0%', '20%'])
   subprocess.call(['sudo', 'mkfs.gfs2', '-O', '-t', 'hacluster:mygfs2', '-p', 'lock_dlm', '-j', '5', '/dev/' + i.split()[0] + '1'])
