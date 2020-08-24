056722949534e233167418ab179eff0f14a8b829
Jason Ju <Jason.Ju@microsoft.com>
2020-08-24 03:27:03 +0000

    Updated node.shared.config.py

--- node.config/node.shared.config.py
+++ node.config/node.shared.config.py
@@ -4,10 +4,10 @@ import subprocess
 import time
 
 lsblk = subprocess.Popen(['lsblk'], stdout=subprocess.PIPE,)
-grep = subprocess.Popen(['grep', '260G'], stdin=lsblk.stdout, stdout=subprocess.PIPE,)
+grep = subprocess.Popen(['grep', '64G'], stdin=lsblk.stdout, stdout=subprocess.PIPE,)
 disk = grep.stdout.readlines()
 id = []
 for i in disk:
   subprocess.call(['sudo', 'parted', '-s', '/dev/' + i.split()[0], 'mklabel', 'gpt'])
-  subprocess.call(['sudo', 'parted', '-s', '-a', 'opt', '/dev/' + i.split()[0], 'mkpart', 'extended', '0%', '20%'])
+  subprocess.call(['sudo', 'parted', '-s', '-a', 'opt', '/dev/' + i.split()[0], 'mkpart', 'extended', '0%', '100%'])
   subprocess.call(['sudo', 'mkfs.gfs2', '-O', '-t', 'hacluster:mygfs2', '-p', 'lock_dlm', '-j', '2', '/dev/' + i.split()[0] + '1'])
