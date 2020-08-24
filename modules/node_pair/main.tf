d54bea86145c603f68d55cb66ae9e69af17aabe3
Jason Ju <Jason.Ju@microsoft.com>
2020-08-24 03:21:27 +0000

    Updated main.tf

--- modules/node_pair/main.tf
+++ modules/node_pair/main.tf
@@ -4,22 +4,22 @@ provider "azurerm" {
 
 resource "null_resource" "shared_disk0" {
   provisioner "local-exec" {
-    command = "az disk create -g ${var.rg} -n shared_disk0 --size-gb 1 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 1000 --disk-mbps-read-write 50"
+    command = "az disk create -g ${var.rg} -n shared_disk0 --size-gb 4 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 100 --disk-mbps-read-write 25"
   }
 }
 resource "null_resource" "shared_disk1" {
   provisioner "local-exec" {
-    command = "az disk create -g ${var.rg} -n shared_disk1 --size-gb 1 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 1000 --disk-mbps-read-write 50"
+    command = "az disk create -g ${var.rg} -n shared_disk1 --size-gb 4 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 100 --disk-mbps-read-write 25"
   }
 }
 resource "null_resource" "shared_disk2" {
   provisioner "local-exec" {
-    command = "az disk create -g ${var.rg} -n shared_disk2 --size-gb 1 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 1000 --disk-mbps-read-write 50"
+    command = "az disk create -g ${var.rg} -n shared_disk2 --size-gb 4 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 100 --disk-mbps-read-write 25"
   }
 }
 resource "null_resource" "shared_disk3" {
   provisioner "local-exec" {
-    command = "az disk create -g ${var.rg} -n shared_disk3 --size-gb 10 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 1000 --disk-mbps-read-write 50"
+    command = "az disk create -g ${var.rg} -n shared_disk3 --size-gb 64 -l ${var.region} --sku UltraSSD_LRS --disk-iops-read-write 240 --disk-mbps-read-write 50"
   }
 }
 
