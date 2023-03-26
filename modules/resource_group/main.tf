6f55b8f2c99661376fdacfa76a395af397e6c703
lasertown <jasonju@gmail.com>
2023-03-26 07:46:00 -0700

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -2,7 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_id" "name" {
+  byte_length = 8
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg
+  name     = "${var.rg}_${random_id.name.hex}"
   location = "westus2"
 }
